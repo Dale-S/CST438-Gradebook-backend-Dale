e01b14be45b28859d966f71b06411c8cf9efb20b
Dale <78234740+Dale-S@users.noreply.github.com>
2023-10-10 13:28:15 -0700

    Assignment4 Changes

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
@@ -3,14 +3,17 @@ package com.cst438.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.FinalGradeDTO;
+import com.cst438.domain.Assignment;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
 import com.cst438.domain.EnrollmentDTO;
@@ -36,7 +39,7 @@ public class RegistrationServiceREST implements RegistrationService {
 	public void sendFinalGrades(int course_id , FinalGradeDTO[] grades) { 
 		
 		//TODO use restTemplate to send final grades to registration service
-		
+		restTemplate.put("/course/{course_id}/finalgrades", grades);
 	}
 	
 	@Autowired
@@ -59,7 +62,18 @@ public class RegistrationServiceREST implements RegistrationService {
 		System.out.println("GradeBook addEnrollment "+enrollmentDTO);
 		
 		//TODO remove following statement when complete.
-		return null;
+		Enrollment en = new Enrollment();
+		Course c = courseRepository.findById(enrollmentDTO.courseId()).orElse(null);
+		if(c == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course with id " + enrollmentDTO.courseId() + " not found.");
+		}
+		else {
+			en.setCourse(c);
+		}
+		en.setStudentEmail(enrollmentDTO.studentEmail());
+		en.setStudentName(enrollmentDTO.studentName());
+		enrollmentRepository.save(en);
+		return (enrollmentDTO);		
 		
 	}
 
