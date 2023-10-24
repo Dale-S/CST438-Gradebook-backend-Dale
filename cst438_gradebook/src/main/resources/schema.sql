3827ef53550ff8e45ede17033d7638371528c653
Dale <78234740+Dale-S@users.noreply.github.com>
2023-10-24 16:51:08 -0700

    Exercise 11 Code Added

--- cst438_gradebook/src/main/resources/schema.sql
+++ cst438_gradebook/src/main/resources/schema.sql
@@ -34,3 +34,26 @@ CREATE TABLE assignment_grade (
   FOREIGN KEY (enrollment_id) REFERENCES enrollment (id) on delete cascade,
   FOREIGN KEY (assignment_id) REFERENCES assignment (id) on delete cascade
 );
+
+create table user_table (
+	id int primary key,  
+	alias varchar(25) unique, 
+	email varchar(25) unique,
+	first_name varchar(25), 
+	last_name varchar(25), 
+	level varchar(25),
+	password varchar(100), 
+	role varchar(25)
+);
+
+create table attempt (
+	id int primary key,
+    answer integer not null, 
+    attempt integer not null, 
+    factora integer not null, 
+    factorb integer not null, 
+    is_correct boolean not null, 
+    user_id bigint, 
+    primary key (id), 
+    foreign key(user_id) references user_table(id)
+);
